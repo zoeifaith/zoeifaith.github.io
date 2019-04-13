e410bf9f0e52964bfadf9b401b1d40ac30523703
Zoei Faith <zoeifaith@Zoeis-MacBook-Air.local>
2019-04-13 18:35:55 -0500

    about page and education page added

--- files/functions.js
+++ files/functions.js
@@ -1,13 +1,25 @@
+$(document).ready(function() {
+  $(".scrollLink").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
 var i = 0;
 function typeWriter() {
     var text = "Welcome to My eResume!";
     var speed = 135;
     if(i < text.length) {
-        if(i == 0) {  
+        if(i == 0) {
             document.getElementById("text").innerHTML = " ";
-            //setTimeout(typeWriter,1000);
         }
         setTimeout(typeWriter, speed);           document.getElementById("text").innerHTML += text.charAt(i);         ++i;
     }
 }
-
