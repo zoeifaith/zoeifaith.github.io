6fcdc6fb9036680935ee7041071b9a6bb2a0263a
Zoei Faith <zoeifaith@Zoeis-MacBook-Air.local>
2019-09-22 01:17:24 -0500

    Updated remaining pages to utilize MDB

--- files/functions.js
+++ files/functions.js
@@ -1,3 +1,12 @@
+// Navbar slide functionality
+document.getElementById("slide-btn").addEventListener("mouseover", function(event) {
+      document.getElementById("slide").style.right = "0px";
+    });
+    document.getElementById("slide").addEventListener("mouseleave", function(event) {
+      document.getElementById("slide").style.right = "-300px";
+    });
+
+/* Adds smooth scroll */
 $(document).ready(function() {
   $(".scrollLink").on('click', function(event) {
     if (this.hash !== "") {
@@ -14,13 +23,22 @@ $(document).ready(function() {
 
 var i = 0;
 function typeWriter() {
-    var text = "Welcome to My eResume!";
-    var speed = 135;
-    if(i < text.length) {
-        if(i == 0) {
-            document.getElementById("text").innerHTML = " ";
-        }
-        setTimeout(typeWriter, speed);           document.getElementById("text").innerHTML += text.charAt(i);         ++i;
+  var text = " Hello, World!";
+  var speed = 200;
+  if(i < text.length) {
+    if(i == 0) {
+      document.getElementById("text").innerHTML = " ";
     }
+    setTimeout(typeWriter, speed);           document.getElementById("text").innerHTML += text.charAt(i);  
+    if(i == text.length-1) {
+      setTimeout(typeWriterReset, 2000);
+    }
+    ++i;
+  }
+}
+
+function typeWriterReset() {
+  i = 0;
+  typeWriter();
 }
 
